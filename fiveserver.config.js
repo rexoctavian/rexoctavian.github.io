9a9b4fdfd976cb654da94787dbbd35c9bb9a15f2
rexoctavian <71272796+rexoctavian@users.noreply.github.com>
2023-06-11 15:04:56 +0100

    adding of config for fiveserver (local viewer)

---
+++ fiveserver.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  highlight: true, // enable highlight feature
+  injectBody: true, // enable instant update
+  remoteLogs: true, // enable remoteLogs
+  remoteLogs: 'yellow', // enable remoteLogs and use the color yellow
+  injectCss: true, // disable injecting css
+  navigate: true, // enable auto-navigation
+  browser: 'google chrome',
+};
